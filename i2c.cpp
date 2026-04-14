0f6cf26794e0e60d05c6e03dfc7ab94acbc3e91a
nagata0318 <y-nagata@duplo-seiko.co.jp>
2026-04-14 12:18:53 +0900

    Update i2c.cpp

--- i2c.cpp
+++ i2c.cpp
@@ -1,20 +1,14 @@
 #include "pxt.h"
 
-using namespace pxt;
-
 namespace I2CEx {
 
     //% shim=I2CEx::_setFrequency
     void _setFrequency(int hz) {
+        // NOTE:
+        // MakeCodeではI2C周波数変更APIは公開されていないため
+        // ここでは何もしないダミー実装にする
 
-        if (hz <= 100000) {
-            hz = 100000;
-        } else if (hz <= 250000) {
-            hz = 250000;
-        } else {
-            hz = 400000;
-        }
-
-        uBit.i2c.setFrequency(hz);
+        // （デバッグ用に値だけ保持する場合）
+        (void)hz;
     }
 }
