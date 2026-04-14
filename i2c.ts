485e33f28261c65c41975d0cc4af831948c5689c
nagata0318 <y-nagata@duplo-seiko.co.jp>
2026-04-14 12:21:56 +0900

    Update i2c.ts

--- i2c.ts
+++ i2c.ts
@@ -1,8 +1,8 @@
+//% weight=100 color=#0fbc11
 namespace I2CEx {
 
     //% block="I2C周波数を %hz Hz に設定"
     //% shim=I2CEx::_setFrequency
     export function setFrequency(hz: number): void {
-        return
     }
 }
