343dfb6020ebdb00b788f071739453ef9c0a30af
nagata0318 <y-nagata@duplo-seiko.co.jp>
2026-04-14 12:22:14 +0900

    Update i2c.cpp

--- i2c.cpp
+++ i2c.cpp
@@ -2,13 +2,7 @@
 
 namespace I2CEx {
 
-    //% shim=I2CEx::_setFrequency
     void _setFrequency(int hz) {
-        // NOTE:
-        // MakeCodeではI2C周波数変更APIは公開されていないため
-        // ここでは何もしないダミー実装にする
-
-        // （デバッグ用に値だけ保持する場合）
         (void)hz;
     }
 }
